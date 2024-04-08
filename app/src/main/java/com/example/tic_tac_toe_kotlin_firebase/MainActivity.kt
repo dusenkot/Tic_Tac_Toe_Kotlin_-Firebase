4cb872346e13324ab75886f4612a036f69bd8c6e
dusenkot <dusenkot@gmail.com>
2024-04-09 01:10:49 +0200

    add new window and start write game

--- app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.tic_tac_toe_kotlin_firebase
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tic_tac_toe_kotlin_firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playOfflineBtn.setOnClickListener{
+            createOfflineGame()
         }
     }
+    private fun createOfflineGame(){
+        startGame()
+    }
+    private fun startGame(){
+        startActivity(Intent(this,GameActivity::class.java))
+    }
+
 }
