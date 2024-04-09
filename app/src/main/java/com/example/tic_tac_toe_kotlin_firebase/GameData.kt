a8133768fa17612e092529add0bbab8f96131cab
dusenkot <dusenkot@gmail.com>
2024-04-10 00:26:05 +0200

    add gameData and GameModel fun

---
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/GameData.kt
@@ -0,0 +1,13 @@
+package com.example.tic_tac_toe_kotlin_firebase
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+object GameData {
+    private var _gameModel : MutableLiveData<GameModel> = MutableLiveData()
+    var gameModel : LiveData<GameModel> = _gameModel
+
+    fun saveGameModel(model: GameModel){
+        _gameModel.postValue(model)
+    }
+}
