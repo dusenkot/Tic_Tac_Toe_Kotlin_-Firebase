135f86d6d31b0b3a6c18e056948222eb81c8f3c7
OstapMaksymiv <ostaperad@gmail.com>
2024-04-12 00:41:59 +0200

    init:project

--- app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/GameModel.kt
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/GameModel.kt
@@ -2,17 +2,18 @@ package com.example.tic_tac_toe_kotlin_firebase
 
 import kotlin.random.Random
 
-data  class GameModel (
+data class GameModel (
     var gameId : String = "-1",
     var filledPos : MutableList<String> = mutableListOf("","","","","","","","",""),
-    var winner : String = "",
+    var winner : String ="",
     var gameStatus : GameStatus = GameStatus.CREATED,
-    var curentPlayer : String = (arrayOf("X","O"))[Random.nextInt(until = 2)]
+    var currentPlayer : String = (arrayOf("X","O"))[Random.nextInt(2)]
 )
+
 
 enum class GameStatus{
     CREATED,
     JOINED,
-    INPROGRES,
+    INPROGRESS,
     FINISHED
 }
