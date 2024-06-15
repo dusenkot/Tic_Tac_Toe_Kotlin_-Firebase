4df9efc249806c77e608a5f8f8e59392fb30393d
Kaellji <Mateuszcislo23@gmail.com>
2024-06-15 21:24:09 +0200

    Snake longer when eat + game over if snake crash and better UI

--- app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/GameActivitySnake.kt
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/GameActivitySnake.kt
@@ -1,43 +1,78 @@
 package com.example.tic_tac_toe_kotlin_firebase
 
+import android.app.Dialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.view.AbsSavedState
 import android.view.KeyEvent
-import android.view.View
+import android.view.WindowManager
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tic_tac_toe_kotlin_firebase.databinding.ActivityGameBinding
-import android.view.Window;
-import android.view.WindowManager;
 
 
-class GameActivitySnake : AppCompatActivity() {
+class GameActivitySnake : AppCompatActivity(), SnakeGameView.GameOverListener {
+
+    companion object {
+
+        @kotlin.jvm.JvmField
+        var dialogScore: Dialog? = null
+        var txtScore: TextView? = null
+        var txtBestScore: TextView? = null
+        var txtDialogScore: TextView? = null
+        var txtDialogBestScore: TextView? = null
+    }
+
+
     private var gameView: SnakeGameView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        // Retrieve display metrics to get screen dimensions
         val dm = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(dm)
-        ConstantsSnake.SCREEN_HEIGHT = dm.heightPixels
         ConstantsSnake.SCREEN_WIDTH = dm.widthPixels
+        ConstantsSnake.SCREEN_HEIGHT = dm.heightPixels
 
         setContentView(R.layout.activity_game_snake)
+
         gameView = findViewById(R.id.gv)
+        txtScore = findViewById(R.id.txt_score)
+        txtBestScore = findViewById(R.id.txt_best_score)
 
-        // Ensure the GameView is focusable and set up to receive key events
-        gameView?.isFocusable = true
-        gameView?.isFocusableInTouchMode = true
         gameView?.requestFocus()
+
+
+        dialogScore()
+
     }
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        // Pass the key event to the GameView for handling
-        return gameView?.onKeyDown(keyCode, event) ?: false
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        return gameView?.onKeyDown(keyCode, event) ?: super.onKeyDown(keyCode, event)
     }
+
+    private fun dialogScore() {
+        val sp: SharedPreferences = getSharedPreferences("gamesetting", Context.MODE_PRIVATE)
+        val bestScore = sp.getInt("bestscore", 0)
+        txtBestScore?.text = bestScore.toString()
+
+        dialogScore = Dialog(this).apply {
+            setContentView(R.layout.dialog_start)
+            txtDialogScore = findViewById(R.id.txt_dialog_score)
+            txtDialogBestScore = findViewById(R.id.txt_dialog_best_score)
+            txtDialogBestScore?.text = bestScore.toString()
+            setCanceledOnTouchOutside(false)
+            findViewById<RelativeLayout>(R.id.rl_start).setOnClickListener {
+                gameView?.reset()
+                dismiss()
+            }
+        }
+        dialogScore?.show()
+    }
+    override fun onGameOver() {
+        dialogScore()
+    }
+
 }
--- app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/SnakeGameView.java
+++ app/src/main/java/com/example/tic_tac_toe_kotlin_firebase/SnakeGameView.java
@@ -1,6 +1,7 @@
 package com.example.tic_tac_toe_kotlin_firebase;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -11,6 +12,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.MotionEvent;
 import android.widget.Switch;
+import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
@@ -22,7 +24,7 @@ import javax.annotation.Nullable;
 
 public class SnakeGameView extends View {
     private Bitmap bmGrass1, bmGrass2, bmSnake, bmApple;
-    public static int sizeOfMap = 75*ConstantsSnake.SCREEN_WIDTH/1080;
+    public static int sizeOfMap = 75 * ConstantsSnake.SCREEN_WIDTH / 1080;
     private int h = 21, w = 12;
     private ArrayList<SnakeGrass> arrGrass = new ArrayList<>();
     private Snake snake;
@@ -31,29 +33,36 @@ public class SnakeGameView extends View {
     private Handler handler;
     private Runnable r;
     private Apple apple;
+    public static int score = 0, bestScore = 0;
+    private Context context;
+    public static boolean isPlaying = false;
+    private GameOverListener gameOverListener;
+
     public SnakeGameView(Context context, @Nullable AttributeSet attrs) {
-        super(context,attrs);
+        super(context, attrs);
+        this.context = context;
+        SharedPreferences sp = context.getSharedPreferences("gamesetting", Context.MODE_PRIVATE);
+        if (sp != null) {
+            bestScore = sp.getInt("bestscore", 0);
+        }
         bmGrass1 = BitmapFactory.decodeResource(this.getResources(), R.drawable.grass);
-        bmGrass1 = Bitmap.createScaledBitmap(bmGrass1,sizeOfMap, sizeOfMap,true );
+        bmGrass1 = Bitmap.createScaledBitmap(bmGrass1, sizeOfMap, sizeOfMap, true);
         bmGrass2 = BitmapFactory.decodeResource(this.getResources(), R.drawable.grass03);
-        bmGrass2 = Bitmap.createScaledBitmap(bmGrass2,sizeOfMap, sizeOfMap,true );
+        bmGrass2 = Bitmap.createScaledBitmap(bmGrass2, sizeOfMap, sizeOfMap, true);
         bmSnake = BitmapFactory.decodeResource(this.getResources(), R.drawable.snake1);
-        bmSnake = Bitmap.createScaledBitmap(bmSnake,14*sizeOfMap, sizeOfMap,true );
+        bmSnake = Bitmap.createScaledBitmap(bmSnake, 14 * sizeOfMap, sizeOfMap, true);
         bmApple = BitmapFactory.decodeResource(this.getResources(), R.drawable.apple);
-        bmApple = Bitmap.createScaledBitmap(bmApple,sizeOfMap, sizeOfMap,true );
-
-        int xOffset = (ConstantsSnake.SCREEN_WIDTH - w * sizeOfMap) / 2;
-        int yOffset = (ConstantsSnake.SCREEN_HEIGHT - h * sizeOfMap) / 2;
-
-        for(int i = 0; i < h; i++){
+        bmApple = Bitmap.createScaledBitmap(bmApple, sizeOfMap, sizeOfMap, true);
+        for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                Bitmap bm = (i + j) % 2 == 0 ? bmGrass1 : bmGrass2;
-                int x = j * sizeOfMap + xOffset;
-                int y = i * sizeOfMap + yOffset;
-                arrGrass.add(new SnakeGrass(bm, x, y, sizeOfMap, sizeOfMap));
+                if ((j + i) % 2 == 0) {
+                    arrGrass.add(new SnakeGrass(bmGrass1, j * bmGrass1.getWidth() + ConstantsSnake.SCREEN_WIDTH / 2 - (w / 2) * bmGrass1.getWidth(), i * bmGrass1.getHeight() + 50 * ConstantsSnake.SCREEN_HEIGHT / 1920, bmGrass1.getWidth(), bmGrass1.getHeight()));
+                } else {
+                    arrGrass.add(new SnakeGrass(bmGrass2, j * bmGrass2.getWidth() + ConstantsSnake.SCREEN_WIDTH / 2 - (w / 2) * bmGrass2.getWidth(), i * bmGrass2.getHeight() + 50 * ConstantsSnake.SCREEN_HEIGHT / 1920, bmGrass2.getWidth(), bmGrass2.getHeight()));
+                }
             }
         }
-        snake = new Snake(bmSnake, arrGrass.get(126).getX(), arrGrass.get(126).getY(),4);
+        snake = new Snake(bmSnake, arrGrass.get(126).getX(), arrGrass.get(126).getY(), 4);
         apple = new Apple(bmApple, arrGrass.get(randomApple()[0]).getX(), arrGrass.get(randomApple()[1]).getY());
         handler = new Handler();
         r = new Runnable() {
@@ -63,66 +72,42 @@ public class SnakeGameView extends View {
             }
         };
     }
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_DPAD_UP:
-                if (!snake.isMove_bottom()) {
-                    snake.setMove_top(true);
-                    return true;
-                }
-                break;
-            case KeyEvent.KEYCODE_DPAD_DOWN:
-                if (!snake.isMove_top()) {
-                    snake.setMove_bottom(true);
-                    return true;
-                }
-                break;
-            case KeyEvent.KEYCODE_DPAD_LEFT:
-                if (!snake.isMove_right()) {
-                    snake.setMove_left(true);
-                    return true;
-                }
-                break;
-            case KeyEvent.KEYCODE_DPAD_RIGHT:
-                if (!snake.isMove_left()) {
-                    snake.setMove_right(true);
-                    return true;
-                }
-                break;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         int a = event.getActionMasked();
-        switch(a){
-            case MotionEvent.ACTION_MOVE:{
-                if (move == false){
+        switch (a) {
+            case MotionEvent.ACTION_MOVE: {
+                if (move == false) {
                     mx = event.getX();
                     my = event.getY();
                     move = true;
-                }else{
-                    if (mx - event.getX()>100*ConstantsSnake.SCREEN_WIDTH/1080 && !snake.isMove_right()){
+                } else {
+                    if (mx - event.getX() > 100 && !snake.isMove_right()) {
                         mx = event.getX();
                         my = event.getY();
-                        snake.setMove_right(true);
-                    }else if (event.getX() - mx>100*ConstantsSnake.SCREEN_WIDTH/1080 && !snake.isMove_left()){
+                        this.snake.setMove_left(true);
+                        isPlaying = true;
+                    } else if (event.getX() - mx > 100 && !snake.isMove_left()) {
                         mx = event.getX();
                         my = event.getY();
-                        snake.setMove_left(true);
-                    }else if (my - event.getY()>100*ConstantsSnake.SCREEN_WIDTH/1080 && !snake.isMove_bottom()){
+                        this.snake.setMove_right(true);
+                        isPlaying = true;
+                    } else if (event.getY() - my > 100 && !snake.isMove_top()) {
                         mx = event.getX();
                         my = event.getY();
-                        snake.setMove_top(true);
-                    }else if (event.getY() - my>100*ConstantsSnake.SCREEN_WIDTH/1080 && !snake.isMove_top()){
+                        this.snake.setMove_bottom(true);
+                        isPlaying = true;
+                    } else if (my - event.getY() > 100 && !snake.isMove_bottom()) {
                         mx = event.getX();
                         my = event.getY();
-                        snake.setMove_bottom(true);
+                        this.snake.setMove_top(true);
+                        isPlaying = true;
                     }
                 }
                 break;
             }
-            case MotionEvent.ACTION_UP:{
+            case MotionEvent.ACTION_UP: {
                 mx = 0;
                 my = 0;
                 move = false;
@@ -137,38 +122,85 @@ public class SnakeGameView extends View {
         super.draw(canvas);
 //        backgropund color after 0xff
         canvas.drawColor(0xFFb5aa5e);
-        for (int i = 0; i < arrGrass.size(); i++){
-            canvas.drawBitmap(arrGrass.get(i).getBm(), arrGrass.get(i).getX(), arrGrass.get(i).getY(),null);
+        for (int i = 0; i < arrGrass.size(); i++) {
+            canvas.drawBitmap(arrGrass.get(i).getBm(), arrGrass.get(i).getX(), arrGrass.get(i).getY(), null);
         }
-        snake.update();
+        if (isPlaying) {
+            snake.update();
+            if (snake.getArrPartSnake().get(0).getX() < this.arrGrass.get(0).getX()
+                    || snake.getArrPartSnake().get(0).getY() < this.arrGrass.get(0).getY()
+                    || snake.getArrPartSnake().get(0).getY() + sizeOfMap > this.arrGrass.get(this.arrGrass.size() - 1).getY() + sizeOfMap
+                    || snake.getArrPartSnake().get(0).getX() + sizeOfMap > this.arrGrass.get(this.arrGrass.size() - 1).getX() + sizeOfMap) {
+                gameOver();
+            }
+            for (int i = 1; i < snake.getArrPartSnake().size(); i++) {
+                if (snake.getArrPartSnake().get(0).getrBody().intersect(snake.getArrPartSnake().get(i).getrBody())) {
+                    gameOver();
+                }
+            }
+        }
         snake.draw(canvas);
         apple.draw(canvas);
-        if (snake.getArrPartSnake().get(0).getrBody().intersect(apple.getR())){
+        if (snake.getArrPartSnake().get(0).getrBody().intersect(apple.getR())) {
             randomApple();
             apple.reset(arrGrass.get(randomApple()[0]).getX(), arrGrass.get(randomApple()[1]).getY());
+            snake.addPart();
+            score++;
+            //thi is where i want to add the code to update the text_score on my .xml file
         }
         handler.postDelayed(r, 200);
     }
 
-    public int[] randomApple(){
-        int[]xy = new int[2];
+    public int[] randomApple() {
+        int[] xy = new int[2];
         Random r = new Random();
-        xy[0] = r.nextInt(arrGrass.size()-1);
-        xy[1] = r.nextInt(arrGrass.size()-1);
-        Rect rect = new Rect(arrGrass.get(xy[0]).getX(), arrGrass.get(xy[1]).getY(),arrGrass.get(xy[0]).getX()+sizeOfMap, arrGrass.get(xy[1]).getY()+sizeOfMap);
+        xy[0] = r.nextInt(arrGrass.size() - 1);
+        xy[1] = r.nextInt(arrGrass.size() - 1);
+        Rect rect = new Rect(arrGrass.get(xy[0]).getX(), arrGrass.get(xy[1]).getY(), arrGrass.get(xy[0]).getX() + sizeOfMap, arrGrass.get(xy[1]).getY() + sizeOfMap);
         boolean check = true;
-        while (check){
+        while (check) {
             check = false;
-            for (int i = 0; i<snake.getArrPartSnake().size(); i++){
-                if (rect.intersect(snake.getArrPartSnake().get(i).getrBody())){
+            for (int i = 0; i < snake.getArrPartSnake().size(); i++) {
+                if (rect.intersect(snake.getArrPartSnake().get(i).getrBody())) {
                     check = true;
-                    xy[0]=r.nextInt(arrGrass.size()-1);
-                    xy[1]=r.nextInt(arrGrass.size()-1);
-                    rect = new Rect(arrGrass.get(xy[0]).getX(), arrGrass.get(xy[1]).getY(),arrGrass.get(xy[0]).getX()+sizeOfMap, arrGrass.get(xy[1]).getY()+sizeOfMap);
+                    xy[0] = r.nextInt(arrGrass.size() - 1);
+                    xy[1] = r.nextInt(arrGrass.size() - 1);
+                    rect = new Rect(arrGrass.get(xy[0]).getX(), arrGrass.get(xy[1]).getY(), arrGrass.get(xy[0]).getX() + sizeOfMap, arrGrass.get(xy[1]).getY() + sizeOfMap);
                 }
             }
 
         }
         return xy;
     }
+
+    private void gameOver() {
+        isPlaying = false;
+        if (gameOverListener != null) {
+            gameOverListener.onGameOver();
+        }
+    }
+
+    public void reset() {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if ((j + i) % 2 == 0) {
+                    arrGrass.add(new SnakeGrass(bmGrass1, j * bmGrass1.getWidth() + ConstantsSnake.SCREEN_WIDTH / 2 - (w / 2) * bmGrass1.getWidth(), i * bmGrass1.getHeight() + 50 * ConstantsSnake.SCREEN_HEIGHT / 1920, bmGrass1.getWidth(), bmGrass1.getHeight()));
+                } else {
+                    arrGrass.add(new SnakeGrass(bmGrass2, j * bmGrass2.getWidth() + ConstantsSnake.SCREEN_WIDTH / 2 - (w / 2) * bmGrass2.getWidth(), i * bmGrass2.getHeight() + 50 * ConstantsSnake.SCREEN_HEIGHT / 1920, bmGrass2.getWidth(), bmGrass2.getHeight()));
+                }
+            }
+        }
+        snake = new Snake(bmSnake, arrGrass.get(126).getX(), arrGrass.get(126).getY(), 4);
+        apple = new Apple(bmApple, arrGrass.get(randomApple()[0]).getX(), arrGrass.get(randomApple()[1]).getY());
+        score = 0;
+    }
+
+    public interface GameOverListener {
+        void onGameOver();
+    }
+
+    public void setGameOverListener(GameOverListener listener) {
+        this.gameOverListener = listener;
+    }
+
 }
